6e738e1d64f1ee3d4dbe2a54f847c023b7309254
Nurdianto <nurdiantopntr@gmail.com>
2016-03-06 15:23:57 +0700

    Update start3.sh

--- start3.sh
+++ start3.sh
@@ -1,2 +1,2 @@
 cd /home/www/domain
-wp post list --post_type=post --fields=ID --format=csv --allow-root | sed -e 's/ID//g' -e '/^$/d' -e 's/"/ > /home/ebay/attachment/data/id.txt
+wp post list --post_type=post --fields=ID --format=csv --allow-root | sed -e 's/ID//g' -e '/^$/d' -e 's/"//g' > /home/ebay/attachment/data/id.txt
